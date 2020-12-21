36661f718b209223e8b93ab303198be2545045a9
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-12-21 21:04:33 +0500

    End of Lesson D

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    
+    counter = session[:counter]
+    
+    if counter.nil?
+      @counter = 1
+    else
+      @counter = counter + 1
+    end
+
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ActiveRecord::Base
-	validates :title, :description, :image_url, presence: true
+	has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
 	validates :title, length: {
@@ -9,11 +13,23 @@ class Product < ActiveRecord::Base
 	validates :image_url, allow_blank: true, format: {
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
-			# URL ������ ��������� �� ����������� ������� GIF, JPG ��� PNG
+			# URL должен указывать на изображение формата GIF, JPG или PNG
 	}
 
   def self.latest
     Product.order(:updated_at).last
+  end
+  
+  private
+
+  # убеждаемся в отсутствии товарных позиций, ссылающихся на данный товар  
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'существуют товарные позиции')
+      return false
+    end
   end
   
 end
