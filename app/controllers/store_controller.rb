1a772e589f9234ee3ad5b047d7090880cae0c286
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-02-07 21:26:51 +0500

    Lesson F - Counter removed from store_controller

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,14 +4,5 @@ class StoreController < ApplicationController
 
   def index
     @products = Product.order(:title)
-    
-    counter = session[:counter]
-    
-    if counter.nil?
-      @counter = 1
-    else
-      @counter = counter + 1
-    end
-
   end
 end
