b1fc352bedf7ddac331a8c552b85a29d73becdef
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-02-09 13:43:13 +0500

    Lesson F - line_items_controller_test fixed

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -20,10 +20,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
       post line_items_url, params: { product_id: products(:ruby).id }
     end
 
-    follow_redirect!
-
-    assert_select 'h2', 'Your Cart'
-    assert_select 'td', "Programming Ruby 1.9"
+    assert_redirected_to store_path
   end
 
   test "should create two unique line_items" do
