6277f11e3327c1f84b41ffa8b59884d2dc5a29a9
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-02-15 14:57:50 +0500

    Lesson G - tests for orders added

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -5,13 +5,24 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
     @order = orders(:one)
   end
 
+  test "requires item in cart" do
+    get :new
+    assert_redirected_to store_path
+    assert_equal flash[:notice], 'Your cart is empty'
+  end
+
   test "should get index" do
     get orders_url
     assert_response :success
   end
 
   test "should get new" do
-    get new_order_url
+    item = LineItem.new
+    item.build_cart
+    item.product = products(:ruby)
+    item.save!
+    session[:cart_id] = item.cart.id
+    get :new
     assert_response :success
   end
 
