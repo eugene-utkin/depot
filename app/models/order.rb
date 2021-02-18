1e3db8de038d137fa436e27a33fe734359788596
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-02-18 13:38:31 +0500

    Lesson G - Validation added to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,7 @@ class Order < ApplicationRecord
 
   PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
 
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
+
 end
