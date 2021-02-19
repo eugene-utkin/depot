10cd8b7c6a8abdc572b465fe44abd89f21763b7a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-02-19 14:41:54 +0500

    Lesson G - orders controller modified

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,16 +30,24 @@ class OrdersController < ApplicationController
 
   # POST /orders
   # POST /orders.json
+  
   def create
     @order = Order.new(order_params)
+    @order.add_line_items_from_cart(@cart)
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
-        format.json { render :show, status: :created, location: @order }
+        Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+        format.html { redirect_to store_url, notice:
+          'Thank you for your order.' }
+        format.json { render action: 'show', status: :created,
+          location: @order }
       else
-        format.html { render :new }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        @cart = current_cart
+        format.html { render action: 'new' }
+        format.json { render json: @order.errors,
+          status: :unprocessable_entity }
       end
     end
   end
