f7cc7a9c46993b49e03f749c5eccf4946e4d1931
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-02-26 15:55:13 +0500

    Lesson H - Setting action_mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+ 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "domain.of.sender.net",
+    authentication: "plain",
+    user_name: "dave",
+    password: "secret",
+    enable_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
