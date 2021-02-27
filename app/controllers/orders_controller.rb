01aa8f8c438d7188ee40f0c1e31d63ad3b315201
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-02-27 14:38:59 +0500

    Lesson H - confirmation email is sent after order creation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -39,12 +39,12 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        OrderNotifier.received(@order).deliver
         format.html { redirect_to store_url, notice:
           'Thank you for your order.' }
         format.json { render action: 'show', status: :created,
           location: @order }
       else
-        @cart = current_cart
         format.html { render action: 'new' }
         format.json { render json: @order.errors,
           status: :unprocessable_entity }
