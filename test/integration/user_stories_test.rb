0dc527c9e8d13c5899597e3755212b8d897a9e0a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-03-02 14:46:40 +0500

    Lesson H - user_story_test modified

--- test/integration/user_stories_test.rb
+++ test/integration/user_stories_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UserStoriesTest < ActionDispatch::IntegrationTest
+  fixtures :products
+
+  LineItem.delete_all
+  Order.delete_all
+  ruby_book = products(:ruby)
   # test "the truth" do
   #   assert true
   # end
