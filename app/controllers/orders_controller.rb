0e15305fe8b4c654d23cca3d544b8f9d23517c7d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-03-06 19:10:27 +0500

    Lesson F - ensure cart isn't empty method added to order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:new, :create]
+  before_action :ensure_cart_isnt_empty, only: :new
   before_action :set_order, only: [:show, :edit, :update, :destroy]
 
   # GET /orders
@@ -77,6 +78,13 @@ class OrdersController < ApplicationController
   end
 
   private
+
+    def ensure_cart_isnt_empty
+      if @cart.line_items.empty?
+        redirect_to store_index_url, notice: 'Your cart is empty'
+      end
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_order
       @order = Order.find(params[:id])
