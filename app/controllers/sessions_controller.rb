dc99c846f1d26e50d03f146fcd35f72d3e291bac
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-03-10 15:58:10 +0500

    Lesson I - password check and redirect to admin added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(name: params[:name])
+    if user and user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_url
+    else
+      redirect_to login_url, alert: "Неверная комбинация имени и пароля"
+    end
   end
 
   def destroy
