9c85282b6e6193b17d3932469e9a54b5d9ff82b8
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-03-12 16:14:20 +0500

    Lesson I - OrdersControllerTest fixed

--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -1,7 +1,11 @@
-test "should create order" do
-  assert_difference('Order.count') do
-    post :create, order: { address: @order.address, email: @order.email,
-      name: @order.name, pay_type: @order.pay_type }
+require 'test_helper'
+
+class OrdersControllerTest < ActionController::TestCase
+  test "should create order" do
+    assert_difference('Order.count') do
+      post :create, order: { address: @order.address, email: @order.email,
+        name: @order.name, pay_type: @order.pay_type }
+    end
+    assert_redirected_to store_path
   end
-  assert_redirected_to store_path
 end
