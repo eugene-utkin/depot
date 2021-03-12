d3e8995fe67cd9cc895cc16c2e7e74a577ddee11
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-03-12 16:28:47 +0500

    Lesson I - authorization checked before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
-before_action :check_time
+before_action :check_time, :authorize
 
   def check_time
     @time = Time.now
+  end
+
+  protected
+
+    def authorize
+      unless User.find_by(id: session[:user_id])
+      redirect_to login_url, notice: "Пожалуйста, пройдите авторизацию"
+    end
   end
 end
