658cf30726dd2e84427bb2b14936529bb7770c17
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-03-20 20:02:28 +0500

    Lesson J - last admin check added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_secure_password
+  after_destroy :ensure_an_admin_remains
+
+  private
+  
+  def ensure_an_admin_remains
+    if User.count.zero?
+      raise "Последний пользователь не может быть удален"
+    end
+  end
+
 end
