8abfaa81a8f5acb424fabae28393e9ed46aca030
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-03-24 16:31:05 +0500

    Lesson K - redirect after choosing a locale added

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,6 +4,11 @@ class StoreController < ApplicationController
   before_action :set_cart
 
   def index
-    @products = Product.order(:title)
+    if params[:set_locale]
+      redirect_to store_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+    end
   end
+  
 end
